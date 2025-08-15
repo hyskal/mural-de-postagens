c504f2101582da8a785a4db0be98d6854a40d4e3
hyskal <36143223+hyskal@users.noreply.github.com>
2025-08-15 08:38:30 -0300

    Atualizar o posts.js

--- api/posts.js
+++ api/posts.js
@@ -1,6 +1,5 @@
 const { Pool } = require('pg');
 
-// Use a variável de ambiente para a string de conexão
 const pool = new Pool({
     connectionString: process.env.NEON_CONNECTION_STRING,
 });
@@ -8,7 +7,7 @@ const pool = new Pool({
 export default async function handler(request, response) {
     try {
         const client = await pool.connect();
-        
+
         if (request.method === 'GET') {
             const { rows } = await client.query('SELECT * FROM memorial_schema.memorial ORDER BY post_date DESC');
             response.status(200).json(rows);
@@ -20,6 +19,19 @@ export default async function handler(request, response) {
             `;
             await client.query(query, [title, image_url, description, author, post_date]);
             response.status(201).json({ message: 'Postagem criada com sucesso!' });
+        } else if (request.method === 'DELETE') {
+            const postId = request.query.id;
+            if (!postId) {
+                return response.status(400).json({ message: 'ID da postagem não fornecido.' });
+            }
+            const query = 'DELETE FROM memorial_schema.memorial WHERE id = $1';
+            const result = await client.query(query, [postId]);
+
+            if (result.rowCount === 0) {
+                return response.status(404).json({ message: 'Postagem não encontrada.' });
+            }
+
+            response.status(200).json({ message: 'Postagem excluída com sucesso!' });
         } else {
             response.status(405).json({ message: 'Método não permitido' });
         }
